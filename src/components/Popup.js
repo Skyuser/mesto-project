413b41ac928d4d6489af0910e5c07efc78d026b1
Natalia Vorobyova <nplyonk@gmail.com>
2023-05-18 09:39:30 +0500

    fix: PopupWithForm

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,9 +1,11 @@
+// Создайте класс Popup, который отвечает за открытие и закрытие попапа.
 export default class Popup {
+  // Принимает в конструктор единственный параметр — селектор попапа.
   constructor(selector) {
-      this._selector = selector;
-      this._popup = document.querySelector(selector);
+    this._selector = selector;
+    this._popup = document.querySelector(selector);
   }
-
+  // Содержит публичные методы open и close, которые отвечают за открытие и закрытие попапа.
   open() {
     this._popup.classList.add("popup_opened");
   }
@@ -11,6 +13,7 @@ export default class Popup {
     this._popup.classList.remove("popup_opened");
   }
 
+  // Содержит приватный метод _handleEscClose, который содержит логику закрытия попапа клавишей Esc.
   _handleEscClose() {
     if (evt.key === "Escape") {
       //   const openedPopup = document.querySelector(".popup_opened");
@@ -19,12 +22,15 @@ export default class Popup {
   }
 
   setEventListeners() {
+    document.addEventListener("keydown", this._handleEscClose());
     this._popup.addEventListener("mousedown", (evt) => {
       if (evt.target.classList.contains("popup__close")) {
         this.close();
+        document.removeEventListener("keydown", this._handleEscClose());
       }
       if (evt.target === this._popup) {
         this.close();
+        document.removeEventListener("keydown", this._handleEscClose());
       }
     });
   }
