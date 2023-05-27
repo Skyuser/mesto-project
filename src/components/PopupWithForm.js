ad4396d1ab4d2276979404c2ec4ddba591151398
Natalia Vorobyova <nplyonk@gmail.com>
2023-05-27 18:13:14 +0500

    fix: remove listener button submit

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -12,15 +12,15 @@ export default class PopupWithForm extends Popup {
 
     // говорим, что эта форма - это форма из попапа (родительского класса)
     this._form = this._popup.querySelector(settings.formSelector);
-    
+
     // находим все инпуты из формы, которую нашли выше
     this._inputList = this._form.querySelectorAll(settings.inputSelector);
-    
+
     // находим кнопку submit
     this._submitButton = this._form.querySelector(
       settings.submitButtonSelector
     );
-    
+
     // находим изначальный текст кнопки submit
     this._submitButtonDefaultText = this._submitButton.textContent;
   }
@@ -34,23 +34,25 @@ export default class PopupWithForm extends Popup {
     return inputValues;
   }
 
+  _submit = (evt) => {
+    this._submitButton.textContent = "Сохранение...";
+    evt.preventDefault();
+    this._submitForm(this._getInputValues());
+  };
   // Перезаписывает родительский метод setEventListeners. Метод setEventListeners класса PopupWithForm должен не только добавлять обработчик клика иконке закрытия, но и добавлять обработчик сабмита формы.
   setEventListeners() {
     //  берем слушатели из Popup
     super.setEventListeners();
     // Добавляем обработчик сабмита формы
-    this._form.addEventListener("submit", (evt) => {
-      this._submitButton.textContent = "Сохранение...";
-      evt.preventDefault();
-      this._submitForm(this._getInputValues());
-    });
+    this._form.addEventListener("submit", this._submit);
   }
-  
+
   // Перезаписывает родительский метод close, так как при закрытии попапа форма должна ещё и сбрасываться.
   close() {
     super.close();
     this._form.reset();
     this._submitButton.textContent = this._submitButtonDefaultText;
+    this._form.removeEventListener("submit", this._submit);
   }
   // Для каждого попапа создавайте свой экземпляр класса PopupWithForm - TO DO
 }
